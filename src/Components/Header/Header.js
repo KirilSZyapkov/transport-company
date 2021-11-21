590d4ae357401e31e0c382b32f78e077de56b944
Kiril Zyapkov <ziapkov@gmail.com>
2021-11-21 19:11:10 +0200

    Styling the header and adding functionality for the banners.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,55 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.css';
 
 
 function Header() {
+
+    const [state, setState] = useState(0);
+
+    const banner = [
+        {
+            first: '/static/transport-banner.jpg'
+        },
+        {
+            second: '/static/transport-banner-two.jpg'
+        },
+        {
+            third: '/static/transport-banner-three.jpg'
+        },
+    ]
+
+    let currentBanner = '';
+
+    function decrease(e) {
+        setState(oldState => {
+            oldState = oldState - 1;
+            if (oldState < 0) {
+                oldState = banner.length - 1;
+            }
+            return oldState;
+        });
+    }
+
+    function increase(e) {
+        setState(oldState => {
+            oldState = oldState + 1;
+            if (oldState > banner.length - 1) {
+                oldState = 0;
+            }
+            return oldState;
+        });
+    }
+
+    if (state === 0) {
+        currentBanner = banner[state].first;
+    } else if (state === 1) {
+        currentBanner = banner[state].second;
+    } else if (state === 2) {
+        currentBanner = banner[state].third;
+    }
+
     return (
         <section className='header'>
             <nav className='header_navigation'>
@@ -12,29 +58,45 @@ function Header() {
                     <Link to="/">
                         <div className='dropdown_uslugi'>
                             <li className='header_navigation_item'>
-                                УСЛУГИ
+                                УСЛУГИ <i className="fas fa-angle-down"></i>
                             </li>
-                            <div class="dropdown_uslugi_content">
+                            <div className="dropdown_uslugi_content">
                                 <ul>
-                                    <li><a href="#">Link 1</a></li>
-                                    <li><a href="#">Link 2</a></li>
-                                    <li><a href="#">Link 3</a></li>
+                                    <li><Link to="/"><span>СУХОПЪТЕН ТРАНСПОРТ</span></Link></li>
+                                    <li><Link to="/"><span>МОРСКИ ТРАНСПОРТ</span></Link></li>
+                                    <li><Link to="/"><span>ВЪЗДУШЕН ТРАНСПОРТ</span></Link></li>
                                 </ul>
                             </div>
                         </div>
                     </Link>
                     <Link to="/">
-                        <li className='header_navigation_item'>
-                            КОМПАНИЯ
-                        </li>
+                        <div className='dropdown_company'>
+                            <li className='header_navigation_item'>
+                                КОМПАНИЯ <i className="fas fa-angle-down"></i>
+                            </li>
+                            <div className="dropdown_company_content">
+                                <ul>
+                                    <li><Link to="/"><span>БИЗНЕС ФИЛОСОФИЯ</span></Link></li>
+                                    <li><Link to="/"><span>УСТОЙЧИВОСТ</span></Link></li>
+                                </ul>
+                            </div>
+                        </div>
                     </Link><Link to="/">
                         <li className='header_navigation_item'>
                             ПАРТНЬОРИ
                         </li>
                     </Link><Link to="/">
-                        <li className='header_navigation_item'>
-                            КАРИЕРА
-                        </li>
+                        <div className="dropdown_cariera">
+                            <li className='header_navigation_item'>
+                                КАРИЕРА <i className="fas fa-angle-down"></i>
+                            </li>
+                            <div className="dropdown_cariera_content">
+                                <ul>
+                                    <li><Link to="#"><span>КАРИЕРА В ТРАНСПОРТА</span></Link></li>
+                                    <li><Link to="#"><span>МЛАД СПЕЦИАЛИСТ</span></Link></li>
+                                </ul>
+                            </div>
+                        </div>
                     </Link><Link to="/">
                         <li className='header_navigation_item'>
                             НОВИНИ
@@ -46,7 +108,11 @@ function Header() {
                     </Link>
                 </ul>
             </nav>
-
+            <article className='header_baner'>
+                <i className="fas fa-chevron-left arrow" onClick={decrease}></i>
+                <i className="fas fa-chevron-right arrow" onClick={increase}></i>
+                <img className='banner' src={currentBanner} alt="banner" />
+            </article>
         </section>
     );
 }
