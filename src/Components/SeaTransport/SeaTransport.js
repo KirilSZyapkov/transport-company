72a967ed9ebaee7e90ce9ff849d9bd8a022729d4
Kiril Zyapkov <ziapkov@gmail.com>
2021-12-05 20:32:12 +0200

    Filling the sea transport page.

--- src/Components/SeaTransport/SeaTransport.js
+++ src/Components/SeaTransport/SeaTransport.js
@@ -3,7 +3,42 @@ import './SeaTransport.css';
 function SeaTransport() {
     return (
         <section className='sea-transport'>
-            Sea Transport
+            <section className='sea-tranport-banner'>
+                <img className='sea-tranport-banner-img' src='/static/sea-logistics.jpg' alt='sea-banner' />
+            </section>
+            <section className='sea-transport-content'>
+                <article className='sea-transport-content-text'>
+                    <h1 className='sea-transport-content-h1'>
+                        Гъвкави комбинации от транспорт по Море, Суша и ЖП.
+                    </h1>
+                    <p className='sea-transport-content-p'>
+                        Ние разбираме пътят към Глобализацията и разширяваме нашето портфолио с услуги, за да улесним връзките на нашите Партньори с техните клиенти или доставчици от и за всяка точка на Света.
+                    </p>
+                    <p className='sea-transport-content-p'>
+                        Имайки това предвид, ние усвоихме и интегрирахме нужната експертиза, позволяваща ни да предложим на нашите клиенти нова услуга: МОРСКИ Транспорт.
+                    </p>
+                    <p className='sea-transport-content-p'>
+                        Вече можем да комбинираме и предложим Мултимодални решения: сухопътни, влакови и по море, управлявани експертно от нашите екипи в България, Гърция и Румъния
+                    </p>
+                    <h1 className='sea-transport-content-h1'>
+                        Практични решения от опитни специалисти.
+                    </h1>
+
+                    <p className='sea-transport-content-p'>
+                        Нашата цел е да предоставим идеалните условия за развитие на бизнеса на нашите Партньори. Морският ни отдел разполага с професионалисти с 10+ години опит в морската спедиция, про-активни и с креативен поглед, винаги търсещи най-ефективното решение, за да отговорят на нуждите на бизнеса на нашите Партньори.
+                    </p>
+
+                    <p className='sea-transport-content-p'>
+                        С високо квалифицирано ниво на Английски език, колегите ни са експедитивни в обработката на всички заявки или предоставянето на нужната подкрепа по веригата от услуги (комуникация с доставчици или 3ти страни).
+                    </p>
+                </article>
+            </section>
+
+            <section className='sea-transport-our-advantiges'>
+<article className='sea-transport-our-advantiges-adnvance'>
+
+</article>
+            </section>
         </section>
     );
 }
